3d1aa69489ccdfac3eb58bc7007b398737285372
Volodymyr <47578369+VolodymyrPerun@users.noreply.github.com>
2019-10-29 16:49:21 +0200

    make new class active to navbar component

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = () => {
     return (
         <nav className={style.nav}>
             <ul className="sidebar-item">
-                <li><a className={style.item} href='/Profile'>Profile</a></li>
+                <li><a className={`${style.item} ${style.active}`} href='/Profile'>Profile</a></li>
                 <li><a className={style.item} href='/Messages'>Messages</a></li>
                 <li><a className={style.item} href='/News'>News</a></li>
                 <li><a className={style.item} href='/Music'>Music</a></li>
