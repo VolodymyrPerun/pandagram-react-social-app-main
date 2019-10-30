21ce7a1b366783d9626971e48418429b974edf00
Volodymyr <47578369+VolodymyrPerun@users.noreply.github.com>
2019-10-30 08:31:45 +0200

    make dialogs component

--- src/App.js
+++ src/App.js
@@ -4,14 +4,17 @@ import './App.css';
 import Header from "./Components/Header/Header";
 import Navbar from "./Components/Navbar/Navbar";
 import Profile from "./Components/Profile/Profile";
-
+import Dialogs from "./Components/Dialogs/Dialogs";
 
 const App = () => {
     return (
         <div className='app-wrapper'>
             <Header/>
             <Navbar/>
-            <Profile/>
+            <div className='app-wrapper-content'>
+                <Dialogs/>
+            </div>
+            {/*<Profile/>*/}
         </div>);
 };
 
--- src/Components/Profile/Profile.jsx
+++ src/Components/Profile/Profile.jsx
@@ -4,7 +4,7 @@ import MyPosts from "../MyPosts/MyPosts";
 
 const Profile = () => {
     return (
-        <div className={style.content}>
+        <div>
             <img
                 src='https://images.pexels.com/photos/2104742/pexels-photo-2104742.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'/>
             <div className={style.item}>Ava+description</div>
