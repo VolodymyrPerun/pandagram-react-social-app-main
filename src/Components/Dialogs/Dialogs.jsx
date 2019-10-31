23ed3b2824edc3058372e37f3f2436e4e9051bd4
Volodymyr <47578369+VolodymyrPerun@users.noreply.github.com>
2019-10-31 10:23:22 +0200

    refactoring dialogs and messages

--- src/Components/Dialogs/Dialogs.jsx
+++ src/Components/Dialogs/Dialogs.jsx
@@ -2,37 +2,39 @@ import React from 'react';
 import style from './Dialogs.module.css';
 import {NavLink} from "react-router-dom";
 
+const DialogsItem  = (props) => {
+    let path = '/dialogs/' + props.id;
+    return (
+            <div className={style.dialog}>
+                <NavLink activeClassName={style.active} to={path}>{props.name}</NavLink>
+            </div>
+    )
+};
+
+const MessagesItem  = (props) => {
+    // let path = '/dialogs/' + props.id;
+    return (
+            <div className={style.message}>
+                <div>{props.message}</div>
+            </div>
+    )
+};
+
 const Dialogs = (props) => {
     return (
         <div className={style.dialogs}>
             <img src="https://images.pexels.com/photos/262508/pexels-photo-262508.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" alt="image"/>
                 <div className={style.dialogsItems}>
-                <div className={style.dialog}>
-                    <NavLink activeClassName={style.active} to='/dialogs/1'>Andriy</NavLink>
-                </div>
-                <div className={style.dialog}>
-                    <NavLink activeClassName={style.active} to='/dialogs/2'>Vasil</NavLink>
-                </div>
-                <div className={style.dialog}>
-                    <NavLink activeClassName={style.active} to='/dialogs/3'>Volodymyr</NavLink>
-                </div>
-                <div className={style.dialog}>
-                    <NavLink activeClassName={style.active} to='/dialogs/4'>Victor</NavLink>
-                </div>
+                <DialogsItem name='Andriy' id='1' />
+                <DialogsItem name='Vasil' id='2' />
+                <DialogsItem name='Volodymyr' id='3' />
+                <DialogsItem name='Victor' id='4' />
             </div>
             <div className={style.messages}>
-                <div className={`${style.message} ${style.active}`}>
-                    Hi, in node we trust!
-                </div>
-                <div className={style.message}>
-                    Yo! Front forever!
-                </div>
-                <div className={style.message}>
-                    What's up! React is top!
-                </div>
-                <div className={style.message}>
-                    Socket.io? Middleware? Token? ... Ease!!!)
-                </div>
+                <MessagesItem message='Hi, in node we trust!'/>
+                <MessagesItem message='Yo! Front forever!'/>
+                <MessagesItem message="What's up! React is top!"/>
+                <MessagesItem message='Socket.io? Middleware? Token? ... Ease!!!)'/>
             </div>
         </div>
     )
