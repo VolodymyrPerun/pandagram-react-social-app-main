c486bdf3a9cbc3a0b7930734147558c0a895bcfd
Volodymyr <47578369+VolodymyrPerun@users.noreply.github.com>
2019-10-31 15:58:16 +0200

    Update MyPosts.jsx

--- src/Components/MyPosts/MyPosts.jsx
+++ src/Components/MyPosts/MyPosts.jsx
@@ -3,6 +3,12 @@ import style from './MyPosts.module.css';
 import Post from './Post/Post';
 
 const MyPosts = () => {
+
+    let postsData = [
+        {id:1, message:'Hi, how are you?', likesCount:'3'},
+        {id:2, message:"It's my first post", likesCount:'45'},
+    ]
+
     return (
         <div className={style.postsBlock}>
            <h3>My posts</h3> 
@@ -15,8 +21,8 @@ const MyPosts = () => {
                 </div>
             </div>
             <div className={style.posts}>
-                <Post message='Hi, how are you?' likesCount='3'/>
-                <Post message="It's my first post" likesCount='45'/>
+                <Post id={postsData[0].id} message={postsData[0].message} likesCount={postsData[0].likesCount}/>
+                <Post id={postsData[1].id} message={postsData[1].message} likesCount={postsData[1].likesCount}/>
             </div>
         </div>
     )
