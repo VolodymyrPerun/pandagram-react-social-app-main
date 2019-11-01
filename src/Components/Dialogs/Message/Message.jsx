57a1dca3f079ce32e6fa76cd626d8a3279a33b6e
Volodymyr <47578369+VolodymyrPerun@users.noreply.github.com>
2019-11-01 14:30:01 +0200

    Update Message.jsx

--- src/Components/Dialogs/Message/Message.jsx
+++ src/Components/Dialogs/Message/Message.jsx
@@ -12,7 +12,6 @@ const MessagesItem = (props) => {
 
 const Message = props => {
 
-
     return props.messagesData.map(message =>
         <MessagesItem id={message.id} message={message.message}/>
     )
