9e761032e8b80c070a3a38de6649dd30cc880875
Volodymyr <47578369+VolodymyrPerun@users.noreply.github.com>
2019-11-09 10:50:39 +0200

    fix Friends Page

--- src/Components/FriendsPage/FriendPageItem/FriendPageItem.jsx
+++ src/Components/FriendsPage/FriendPageItem/FriendPageItem.jsx
@@ -5,16 +5,13 @@ import store from "../../../redux/state";
 const FriendsData = (props) => {
     return (
         <div className={style.friendItem}>
-            <img
-                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTZO6BA3-NDBA6PD0KwkmHUrrQiSqip4PZSBbVE4GGmhxIPfVM&s"
-                alt="avatar"/>
+            <img  src={props.avatar} />
             <div className={style.info}>
                 <div>{'Name:  ' + props.name}</div>
                 <div>{'Surname:  ' + props.surname}</div>
                 <div>{'Age:  ' + props.age}</div>
                 <div>{'Sex:  ' + props.sex}</div>
                 <div>{'Address:  ' + props.address}</div>
-
             </div>
         </div>
     )
@@ -25,9 +22,9 @@ const FriendPageItem = (props) => {
 
 
     props = store.getState().sidebar.friends
-    let friendName = props.map((name, id, age, address, surname, sex) =>
-        <FriendsData id={id.id} name={name.name} surname={name.surname}
-                     age={name.age} address={name.address} sex={name.sex}/>)
+    let friendName = props.map((el) =>
+        <FriendsData id={el.id}  avatar={el.avatar} name={el.name} surname={el.surname}
+                     age={el.age} address={el.address} sex={el.sex}/>)
 
     return (
         <div>
