aad00bafa5d32acbeb5ece9bbd479c351f74b9a1
Volodymyr <47578369+VolodymyrPerun@users.noreply.github.com>
2021-01-03 23:13:41 +0200

    refactored, add friendsPage reducer

--- src/redux/redux-store.js
+++ src/redux/redux-store.js
@@ -2,11 +2,13 @@ import {createStore, combineReducers} from 'redux';
 import reducerProfile from './profile-reducer';
 import reducerDialogs from './dialogs-reducer';
 import reducerSidebar from './sidebar-reducer';
+import reducerFriends from "./friends-reducer";
 
 
 let reducers = combineReducers({
     profilePage: reducerProfile,
     messagesPage: reducerDialogs,
+    friendsPage: reducerFriends,
     sidebar: reducerSidebar
 });
 
