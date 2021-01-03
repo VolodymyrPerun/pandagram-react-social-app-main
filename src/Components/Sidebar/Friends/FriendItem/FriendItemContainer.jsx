a37544fc9c439d56f89ec00c672af9eca50f0a0f
Volodymyr <47578369+VolodymyrPerun@users.noreply.github.com>
2021-01-03 23:14:14 +0200

    Update FriendItemContainer.jsx

--- src/Components/Sidebar/Friends/FriendItem/FriendItemContainer.jsx
+++ src/Components/Sidebar/Friends/FriendItem/FriendItemContainer.jsx
@@ -1,14 +1,20 @@
 import {connect} from 'react-redux';
 import FriendItem from "./FriendItem";
+import {setFriendsActionCreator} from "../../../../redux/friends-reducer";
 
 
 let mapStateToProps = (state) => {
-
     return {
         sidebar: state.sidebar
     }
 };
 
-const FriendItemContainer = connect(mapStateToProps)(FriendItem);
+let mapDispatchToProps = dispatch => {
+    return {
+        setFriends: friend => dispatch(setFriendsActionCreator(friend))
+    }
+}
+
+const FriendItemContainer = connect(mapStateToProps, mapDispatchToProps)(FriendItem);
 
 export default FriendItemContainer;
