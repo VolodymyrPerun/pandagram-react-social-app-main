d410125b661fad13002063eedf80fc8ceaf25918
Volodymyr <47578369+VolodymyrPerun@users.noreply.github.com>
2021-01-20 00:16:27 +0200

    add error styles to FormsControls  inputs

--- src/Components/commons/FormsControls/FormsControls.jsx
+++ src/Components/commons/FormsControls/FormsControls.jsx
@@ -4,28 +4,29 @@ import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faExclamationCircle} from '@fortawesome/free-solid-svg-icons'
 
 const Textarea = ({input, meta: {touched, error, warning}, label, ...props}) => {
-    debugger
+
+    const isError = touched && ((error || warning));
+
     return (
         <>
             {/*<label>{label}</label>*/}
-            <div>
+            <div className={`${style.formsControls} ${isError ? style.error : ""}`}>
                 <textarea className={style.textareaField}
                           {...input}
                           placeholder={props.placeholder}
                           rows="4"
                           cols="50"/>
-                <div className={style.errors}>
-                    {touched &&
-                    ((error && <span>
+                <div className={style.errorsContainer}>
+                    {isError && (<span>
                             <FontAwesomeIcon
                                 style={{marginRight: '3px'}}
                                 icon={faExclamationCircle}/>{
-                            error}</span>) ||
-                        (warning && <span>
+                            error}</span> ||
+                        <span>
                             <FontAwesomeIcon
                                 style={{marginRight: '3px'}}
                                 icon={faExclamationCircle}/>
-                            {warning}</span>))}
+                            {warning}</span>)}
                 </div>
             </div>
         </>
