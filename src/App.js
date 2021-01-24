4bf141265fadbe8ff33255c21ba0d313c6fc83ab
Volodymyr <47578369+VolodymyrPerun@users.noreply.github.com>
2021-01-24 10:46:16 +0200

    transferred authMe() from Header to App, made App class container component

--- src/App.js
+++ src/App.js
@@ -10,29 +10,53 @@ import DialogsContainer from "./Components/Dialogs/DialogsContainer";
 import ProfileContainer from "./Components/Profile/ProfileContainer";
 import HeaderContainer from "./Components/Header/HeaderContainer";
 import Login from "./Components/Login/Login";
+import {connect} from "react-redux";
+import {authMe} from "./redux/auth-reducer";
+import {compose} from "redux";
+import {withAuthRedirect} from "./HOC/withAuthRedirect";
 
 
-const App = () => {
+class App extends React.Component {
 
-    return (
-        <div className='app-wrapper'>
-            <HeaderContainer/>
-            <Sidebar/>
-            <div className='app-wrapper-content'>
-                <Route exact path='/'
-                       render={() => <Redirect to={"/profile"}/>}/>
-                <Route path='/dialogs' render={() =>
-                    <DialogsContainer/>}/>
-                <Route path='/profile/:userId?' render={() =>
-                    <ProfileContainer/>}/>
-                <Route path='/news' render={() => <News/>}/>
-                <Route path='/music' render={() => <Music/>}/>
-                <Route path='/settings' render={() => <Settings/>}/>
-                <Route path='/friendsPage' render={() =>
-                    <FriendsPage/>}/>
-                <Route path='/login' render={() => <Login/>}/>
-            </div>
-        </div>);
-};
+    componentDidMount() {
+        this.props.authMe();
+    };
 
-export default App;
+    render() {
+
+        return (
+            <div className='app-wrapper'>
+                <HeaderContainer/>
+                <Sidebar/>
+                <div className='app-wrapper-content'>
+                    <Route exact path='/'
+                           render={() => <Redirect to={"/profile"}/>}/>
+                    <Route path='/dialogs' render={() =>
+                        <DialogsContainer/>}/>
+                    <Route path='/profile/:userId?' render={() =>
+                        <ProfileContainer/>}/>
+                    <Route path='/news' render={() => <News/>}/>
+                    <Route path='/music' render={() => <Music/>}/>
+                    <Route path='/settings' render={() => <Settings/>}/>
+                    <Route path='/friendsPage' render={() =>
+                        <FriendsPage/>}/>
+                    <Route path='/login' render={() => <Login/>}/>
+                </div>
+            </div>);
+    }
+}
+
+// let mapStateToProps = state => {
+//     return {
+//         isAuth: state.auth.isAuth,
+//         login: state.auth.login
+//     }
+// };
+
+export default compose(
+    // withAuthRedirect,
+    connect(null,
+        {
+            authMe
+        })
+)(App);
--- src/Components/Header/HeaderContainer.jsx
+++ src/Components/Header/HeaderContainer.jsx
@@ -1,13 +1,9 @@
 import React from 'react';
 import Header from "./Header";
 import {connect} from "react-redux";
-import {authMe, logout} from "../../redux/auth-reducer";
+import {logout} from "../../redux/auth-reducer";
 
 class HeaderContainer extends React.Component {
-
-    componentDidMount() {
-        this.props.authMe();
-    };
 
     render() {
         return (
@@ -23,8 +19,4 @@ let mapStateToProps = state => {
     }
 };
 
-export default connect(mapStateToProps,
-    {
-        authMe,
-        logout
-    })(HeaderContainer);
+export default connect(mapStateToProps, {logout})(HeaderContainer);
