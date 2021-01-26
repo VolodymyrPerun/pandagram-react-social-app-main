59e3bfe99d536233018f2cc6a4705f58cb9d0d14
Volodymyr <47578369+VolodymyrPerun@users.noreply.github.com>
2021-01-26 21:10:36 +0200

    add react memo function optimization

--- src/Components/commons/Buttons/Apply/ApplyBtn.jsx
+++ src/Components/commons/Buttons/Apply/ApplyBtn.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import style from './ApplyBtn.module.scss';
 
-const ApplyBtn = props => {
+const ApplyBtn = React.memo(props => {
     return (
         <>
             <button className={style.button} onClick={props.handleClick}>{props.label}</button>
         </>
     )
-};
+});
 
 export default ApplyBtn;
