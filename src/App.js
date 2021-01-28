bf0b62f7d333242aaed72797065e3e4fc9a0fc7d
Volodymyr <47578369+VolodymyrPerun@users.noreply.github.com>
2021-01-28 18:50:23 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -63,7 +63,7 @@ let AppContainer = compose(
     connect(mapStateToProps, {initializeApp}))(App);
 
 let SocialApp = props => {
-    return <BrowserRouter>
+    return <BrowserRouter basename={process.env.PUBLIC_URL}>
         <Provider store={store}>
             <AppContainer/>
         </Provider>
